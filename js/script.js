9669cfefbedde14ac193b09860183a1f7f0138a2
pestao <inertiatic@yandex.ru>
2012-11-21 15:33:47 +0200

    second commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
-    $('[data-hint]').hover(function() {
-        var titleText = $(this).attr('data-hint');
-        $(this).data('tipText', titleText).removeAttr('title');
-        $('<p class="tooltip"></p>').text(titleText).appendTo('body').fadeIn('slow');
-    }, function() {
-        // Hover out code
-        $(this).attr('title', $(this).data('tipText'));
-        $('.tooltip').remove();
-    }).mousemove(function(e) {
-        var mouseX = e.pageX + 20; //Get X coordinates
-        var mouseY = e.pageY + 10; //Get Y coordinates
-        $('.tooltip').css({ top: mouseY, left: mouseX });
-    });
+    (function($) {
+        $.fn.tooltip = function() {
+            $(this).hover(function() {
+                var titleText = $(this).attr('data-hint');
+                $(this).data('tipText', titleText).removeAttr('title');
+                $('<p class="tooltip"></p>').text(titleText).appendTo('body').fadeIn('slow');
+            }, function() {
+                // Hover out code
+                $(this).attr('title', $(this).data('tipText'));
+                $('.tooltip').remove();
+            }).mousemove(function(e) {
+                var mouseX = e.pageX + 20;
+                var mouseY = e.pageY + 10; 
+                $('.tooltip').css({ top: mouseY, left: mouseX });
+            });
+        };
+    })(jQuery);
+    $('div').tooltip();
 });
